36f7e56271d0e35a01dd597c839ddda6f6ed6f70
Kevin Peters <git.igeligel@googlemail.com>
2017-05-24 22:21:06 +0200

    feat(models)
    push first models

---
+++ src/skadisteam.shared/Models/ErrorType.cs
@@ -0,0 +1,23 @@
+﻿namespace skadisteam.shared.Models
+{
+    /// <summary>
+    /// Enumeration to describe errors of the login.
+    /// </summary>
+    public enum ErrorType
+    {
+        /// <summary>
+        /// Given when the twofactor code is wrong.
+        /// </summary>
+        TwoFactor,
+        /// <summary>
+        /// Given when the combination of username and
+        /// password is incorrect.
+        /// </summary>
+        IncorrectLogin,
+        /// <summary>
+        /// Given when there were to many failed attempts
+        /// to login.
+        /// </summary>
+        CaptchaNeeded
+    }
+}
